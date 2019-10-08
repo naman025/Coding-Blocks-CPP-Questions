16190258216d378e421157a825d51fc72ec4b03c
Naman Aggarwal <47495666+naman025@users.noreply.github.com>
2019-10-08 20:40:37 +0530

    Simple Input

---
+++ Fundamentals/simpleInput.cpp
@@ -0,0 +1,39 @@
+/*SIMPLE INPUT*/
+/*
+Given a list of numbers, stop processing input after the cumulative sum of all the input becomes negative.
+
+Input Format:
+A list of integers to be processed
+
+Constraints:
+All numbers input are integers between -1000 and 1000.
+
+Output Format
+Print all the numbers before the cumulative sum become negative.
+
+Sample Input
+1
+2
+88
+-100
+49
+Sample Output
+1
+2
+88
+*/
+
+#include<iostream>
+using namespace std;
+int main() {
+    int n, sum = 0;
+    while(1){
+        cin>>n;
+        sum += n;
+        if(sum<0){
+            break;
+        }
+        cout<<n<<endl;
+    }
+	return 0;
+}
